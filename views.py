18f3775b63038e299c5ea0402882f8936edadd03
Julian Greensmith <julian.greensmith@gmail.com>
2022-11-29 09:55:41 +1100

    Refined web navigation's

--- views.py
+++ views.py
@@ -2,21 +2,23 @@ from flask import Flask, request, redirect, render_template, session, url_for, R
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1000 * 1000
 
 @app.route('/', methods=['GET'])
 def homepage():
-    return render_template('cover.html', home=True)
+    return render_template('index.html', home=True)
 
 @app.route('/', methods=['POST'])
 def post_content():
-    print(request.files['file'])
-    print(request.form['text-input'])
-    return render_template('cover.html', home=True)
+    received_file = request.files['file']
+    filename = secure_filename(received_file.filename)
+    received_text = str(request.form['text-input'])
+    return render_template('index.html', home=True, submitted=True, filename=filename, text=received_text)
 
 @app.route('/about', methods=['GET'])
 def about_page():
-    return render_template('cover.html', about=True)
+    return render_template('index.html', about=True)
 
 @app.route('/samples', methods=['GET'])
 def samples_page():
-    return render_template('cover.html', samples=True)
+    return render_template('index.html', samples=True)
